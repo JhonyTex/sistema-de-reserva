10fb73ea528128c00dd76ea553435b1937cb70e5
JhonyTex <ale3195377386@gmail.com>
2025-06-04 14:57:07 -0500

    Actualizacion

--- sql/suitestar2.sql
+++ sql/suitestar2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2025 a las 03:54:24
+-- Tiempo de generación: 04-06-2025 a las 21:55:08
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
